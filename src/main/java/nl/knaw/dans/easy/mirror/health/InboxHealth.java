285bfefdbf78b6b49b1150ce5c5a03b29a0171a8
Jan van Mansum <janvanmansum@gmail.com>
2022-04-10 21:02:32 +0200

    Added health check

---
+++ src/main/java/nl/knaw/dans/easy/mirror/health/InboxHealth.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2022 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.easy.mirror.health;
+
+import com.codahale.metrics.health.HealthCheck;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+public class InboxHealth extends HealthCheck {
+    private final Path inboxDir;
+
+    public InboxHealth(Path inboxDir) {
+        this.inboxDir = inboxDir;
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        try {
+            Files.list(inboxDir).collect(Collectors.toList());
+        }
+        catch (IOException e) {
+            return Result.unhealthy("Could not read inbox", e);
+        }
+        return Result.healthy();
+    }
+}
